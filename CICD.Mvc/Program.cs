60ee207b920681198eba19913304adf149027c4e
François Joachim <joachim.francois@howest.be>
2023-10-02 10:59:41 +0200

    Testing lock

--- CICD.Mvc/Program.cs
+++ CICD.Mvc/Program.cs
@@ -20,7 +20,7 @@ namespace CICD.Mvc
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>()
+                    webBuilder.UseStartup<Startup>();
                 });
     }
 }
